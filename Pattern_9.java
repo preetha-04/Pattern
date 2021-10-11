22a7aeab3d3d65fade17312c623adcec20f6d719
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-11 20:02:59 +0530

    Add files via upload

---
+++ Pattern_9.java
@@ -0,0 +1,21 @@
+class Pattern_9 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=4; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				if((i+j)%2==1) //it checks remainder
+				{
+					System.out.print(0+" ");
+				}
+				else
+				{
+					System.out.print(1+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
