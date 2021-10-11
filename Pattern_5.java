b293516455561256683239d07d58973f78d1f170
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-11 20:00:21 +0530

    Add files via upload

---
+++ Pattern_5.java
@@ -0,0 +1,15 @@
+class Pattern_5 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=5; i>=1; i--)
+		{
+			for(int j=i; j<=5; j++)
+			{
+				System.out.print(j+ " ");
+			}
+			System.out.println();
+		}
+
+	}
+}
