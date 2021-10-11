344504d7ac2cad4acbfe27bb8a910083b1762295
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-11 20:03:30 +0530

    Add files via upload

---
+++ Pattern_10.java
@@ -0,0 +1,22 @@
+class Pattern_10 
+{
+	public static void main(String[] args) 
+	{
+		int i,j,k;
+	
+		for(i=1; i<=n; i++){
+
+			for(j=1; k=i; j<=i; j++; k-) { 
+			
+			if(k%2!=0) //if we want to know when the number is odd
+			{
+				
+				System.out.print(j+ " ");
+				}
+			}
+		
+		}
+		System.out.println();
+
+	}
+}
